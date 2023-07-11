5871e71af2b7a886823040613f5098fdc4a331b2
fortune710 <fortune0208@yahoo.com>
2023-07-11 12:34:10 +0100

    lazy loaded all pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
 import { Route, Routes } from 'react-router-dom'
-import AdminDashboard from './pages/Admin/Dashboard'
-import StaffTable from './pages/Admin/StaffTable';
-
-
-import './App.css'
-import LoginPage from './pages/Login';
 import { lazy, Suspense } from 'react';
 import Loading from './components/Loading';
+import './App.css'
 
-import Inventory from './pages/Admin/Inventory';
-import PaymentsPage from './pages/Admin/Transactions';
-import Update from './pages/Admin/Products';
-import ViewTransaction from './pages/SeeTransaction';
 
+//import ViewTransaction from './pages/SeeTransaction';
+const LoginPage = lazy(() => import('./pages/Login').then(module => ({ default: module.default })))
 const SalesHomePage = lazy(() => import('./pages/Sales/Home').then(module => ({ default: module.default })))
 const ForgotPassword = lazy(() => import('./pages/ForgotPassword').then(module => ({ default: module.default })));
-
-
 const ProductionsPage = lazy(() => import('./pages/Production/Productions').then(module => ({ default: module.default })))
+
+//Admin Pages
+const AdminDashboard = lazy(() => import('./pages/Admin/Dashboard').then(module => ({ default: module.default })))
+const StaffTable = lazy(() => import('./pages/Admin/StaffTable').then(module => ({ default: module.default })))
+const PaymentsPage = lazy(() => import('./pages/Admin/Transactions').then(module => ({ default: module.default })))
+const Products = lazy(() => import('./pages/Admin/Products').then(module => ({ default: module.default })))
+const Inventory = lazy(() => import('./pages/Admin/Inventory').then(module => ({ default: module.default })))
+const AdminLogin = lazy(() => import('./pages/Admin/Login').then(module => ({ default: module.default })))
 
 
 function App() {
@@ -28,16 +27,15 @@ function App() {
         <Route index path="/" element={<LoginPage/>}/>
         <Route index path="/login" element={<LoginPage/>}/>
         <Route path="/forgot-password" element={<ForgotPassword/>}/>
-
-        <Route path="/transaction/:id" element={<ViewTransaction/>}/>
         
         <Route path='/admin' element={<AdminDashboard/>}>  
           <Route path='users' index element={<StaffTable/>}/>
           <Route path="payments" element={<PaymentsPage/>}/>
           <Route path="inventory" element={<Inventory/>} />
-          <Route path="update" element={<Update/>} />
+          <Route path="update" element={<Products/>} />
         </Route>
 
+        <Route path="/admin-login" element={<AdminLogin/>}/>
         
         <Route path="/sales" element={<SalesHomePage/>}/>
         <Route path="/productions" element={<ProductionsPage/>}/>
